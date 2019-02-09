4a61849fcf42366ba0dc1846207fdd47aa3af5e1
olga <olgaa.yatsuk@gmail.com>
2019-02-09 14:16:07 +0200

    slider;

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,24 @@
-import 'slick-carousel';
+// import 'slick-carousel';
+import 'owl.carousel';
 
+// import Scroll from './anchor-scroll'
+//
+// Scroll();
+//
+//
+// $(function () {
+//   $('.js-nav-btn').on('click', showMenu);
+//   $('.js-close').on('click', hideMenu);
+//
+//   function showMenu() {
+//     $('.js-nav').addClass('is-visible');
+//   }
+//
+//   function hideMenu() {
+//     $('.js-nav').removeClass('is-visible');
+//   }
+// });
 
-import Scroll from './anchor-scroll'
-
-Scroll();
-
-
-$(function () {
-  $('.js-nav-btn').on('click', showMenu);
-  $('.js-close').on('click', hideMenu);
-
-  function showMenu() {
-    $('.js-nav').addClass('is-visible');
-  }
-
-  function hideMenu() {
-    $('.js-nav').removeClass('is-visible');
-  }
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel();
 });
